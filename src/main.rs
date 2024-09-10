3621590241d277b704d3166db5514321ae935c7b
Jonathan Wight <schwa@schwa.io>
2024-09-10 14:51:33 -0700

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use anyhow::{anyhow, Context, Result};
+use anyhow::{anyhow, Context, Ok, Result};
 use clap::{Parser, Subcommand};
 use colored_markup::{println_markup, StyleSheet};
+use git2::RepositoryInitMode;
 use path_absolutize::*;
 use serde::{Deserialize, Serialize};
 use std::collections::{HashMap, HashSet};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use walkdir::WalkDir;
 
 #[derive(Parser)]
 #[clap(name = "My CLI Program")]
@@ -26,20 +28,25 @@ enum Commands {
     // GC {},
     // Info {},
     /// List repositories.
-    List {},
+    List {
+        #[arg(short, long)]
+        filter: Vec<Filter>,
+    },
     // Pull {},
     // Push {},
     /// Register a git repositories or a directories of git repositories
-    Register {
-        paths: Vec<PathBuf>,
-    },
+    Register { paths: Vec<PathBuf> },
     // Reveal {},
-    Status {},
-    // UI {},
-    /// Unregister a git repositories or a directories of git repositories
-    Unregister {
-        paths: Vec<PathBuf>,
+    Status {
+        #[arg(short, long)]
+        filter: Vec<Filter>,
     },
+    UI {
+        #[arg(short, long)]
+        filter: Vec<Filter>,
+    },
+    /// Unregister a git repositories or a directories of git repositories
+    Unregister { paths: Vec<PathBuf> },
 }
 
 fn main() {
@@ -48,17 +55,20 @@ fn main() {
     let mut multigit = Multigit::new().unwrap();
 
     match &cli.command {
-        Commands::List {} => {
-            multigit.list().unwrap();
+        Commands::List { filter } => {
+            multigit.list(noneify(filter)).unwrap();
         }
         Commands::Register { paths } => {
             multigit.register(paths).unwrap();
         }
-        Commands::Status {} => {
-            multigit.status().unwrap();
+        Commands::Status { filter } => {
+            multigit.status(noneify(filter)).unwrap();
         }
         Commands::Unregister { paths } => {
             multigit.unregister(paths).unwrap();
+        }
+        Commands::UI { filter } => {
+            multigit.ui(noneify(filter)).unwrap();
         }
     }
 }
@@ -68,13 +78,71 @@ struct RepositoryEntry {
     path: PathBuf,
 }
 
+#[derive(Debug, Deserialize, Serialize)]
+struct DirectoryEntry {
+    path: PathBuf,
+}
+
+impl RepositoryEntry {
+    fn state(&self) -> Result<RepositoryState> {
+        let mut state = RepositoryState {
+            entries: HashSet::new(),
+        };
+
+        let git_repo = git2::Repository::open(&self.path)?;
+        let mut status_options = git2::StatusOptions::new();
+        status_options.include_untracked(true);
+        status_options.include_ignored(false);
+        let statuses = git_repo.statuses(Some(&mut status_options))?;
+        for status in statuses.into_iter() {
+            match status.status() {
+                git2::Status::INDEX_NEW => {
+                    state.entries.insert(EntryState::Dirty);
+                }
+                git2::Status::INDEX_MODIFIED => {
+                    state.entries.insert(EntryState::Dirty);
+                }
+                git2::Status::INDEX_DELETED => {
+                    state.entries.insert(EntryState::Dirty);
+                }
+                git2::Status::INDEX_RENAMED => {
+                    state.entries.insert(EntryState::Dirty);
+                }
+                git2::Status::INDEX_TYPECHANGE => {
+                    state.entries.insert(EntryState::Dirty);
+                }
+                git2::Status::WT_NEW => {
+                    state.entries.insert(EntryState::Dirty);
+                }
+                git2::Status::WT_MODIFIED => {
+                    state.entries.insert(EntryState::Dirty);
+                }
+                git2::Status::WT_DELETED => {
+                    state.entries.insert(EntryState::Dirty);
+                }
+                git2::Status::WT_TYPECHANGE => {
+                    state.entries.insert(EntryState::Dirty);
+                }
+                git2::Status::WT_RENAMED => {
+                    state.entries.insert(EntryState::Dirty);
+                }
+                git2::Status::CONFLICTED => {
+                    state.entries.insert(EntryState::Dirty);
+                }
+                _ => {}
+            }
+        }
+        Ok(state)
+    }
+}
+
 #[derive(Debug, Deserialize, Serialize, Default)]
 struct Config {
     #[serde(default = "HashMap::new")]
     repositories: HashMap<String, RepositoryEntry>,
 
-    #[serde(default = "HashSet::new")]
-    directories: HashSet<PathBuf>,
+    #[serde(default = "HashMap::new")]
+    directories: HashMap<String, DirectoryEntry>,
 }
 
 impl Config {
@@ -95,15 +163,23 @@ impl Config {
         Ok(())
     }
 
-    fn register(&mut self, path: &PathBuf) -> Result<()> {
+    fn register(&mut self, path: &Path) -> Result<()> {
         let absolute_path = path.absolutize().context("Failed to get absolute path")?;
         let name = absolute_path
             .to_str()
             .context("Failed to convert path to string")?;
-        let repository = RepositoryEntry {
-            path: path.to_path_buf(),
-        };
-        self.repositories.insert(name.to_string(), repository);
+
+        if !is_git_repository(path) {
+            let entry = DirectoryEntry {
+                path: path.to_path_buf(),
+            };
+            self.directories.insert(name.to_string(), entry);
+        } else {
+            let entry = RepositoryEntry {
+                path: path.to_path_buf(),
+            };
+            self.repositories.insert(name.to_string(), entry);
+        }
         self.save()?;
         Ok(())
     }
@@ -113,12 +189,14 @@ impl Config {
         let name = absolute_path
             .to_str()
             .context("Failed to convert path to string")?;
+        self.directories.remove(name);
         self.repositories.remove(name);
         self.save()?;
         Ok(())
     }
 }
 
+#[derive(Debug)]
 struct Multigit {
     config: Config,
     style_sheet: StyleSheet<'static>,
@@ -140,6 +218,46 @@ impl Multigit {
             config,
             style_sheet,
         })
+    }
+
+    fn all_repositories(&self, filter: Option<&Vec<Filter>>) -> Result<Vec<RepositoryEntry>> {
+        let mut repositories: Vec<RepositoryEntry> = Vec::new();
+        for (_, repository) in self.config.repositories.iter() {
+            repositories.push(RepositoryEntry {
+                path: repository.path.clone(),
+            });
+        }
+        for (_, directory) in self.config.directories.iter() {
+            let directory_repositories = find_repositories(&directory.path)?;
+            for repository in directory_repositories {
+                let repository = RepositoryEntry { path: repository };
+                repositories.push(repository);
+            }
+        }
+
+        if let Some(filter) = filter {
+            if !filter.is_empty() {
+                repositories = repositories
+                    .into_iter()
+                    .filter(|repository| {
+                        let state = repository.state().unwrap();
+                        for f in filter {
+                            match f {
+                                Filter::Dirty => {
+                                    if state.entries.contains(&EntryState::Dirty) {
+                                        return true;
+                                    }
+                                }
+                            }
+                        }
+                        return false;
+                    })
+                    .collect();
+            }
+        }
+
+        repositories.sort_by(|a, b| a.path.cmp(&b.path));
+        Ok(repositories)
     }
 
     fn register(&mut self, paths: &Vec<PathBuf>) -> Result<()> {
@@ -166,19 +284,23 @@ impl Multigit {
         Ok(())
     }
 
-    fn list(&self) -> Result<()> {
-        for (_, repository) in self.config.repositories.iter() {
-            println!("{:?}", repository.path);
+    fn list(&self, filter: Option<&Vec<Filter>>) -> Result<()> {
+        for repository in self.all_repositories(filter)?.iter() {
+            println_markup!(
+                &self.style_sheet,
+                "<repopath>{}</repopath>",
+                repository.path.to_str().unwrap(),
+            );
         }
         Ok(())
     }
 
-    fn status(&self) -> Result<()> {
+    fn status(&self, filter: Option<&Vec<Filter>>) -> Result<()> {
         let mut status_options = git2::StatusOptions::new();
         status_options.include_untracked(true);
         status_options.include_ignored(false);
 
-        for (_, repository) in self.config.repositories.iter() {
+        for repository in self.all_repositories(filter)?.iter() {
             let repo = git2::Repository::open(&repository.path)?;
             let status = repo.statuses(Some(&mut status_options))?;
             if !status.is_empty() {
@@ -282,5 +404,69 @@ impl Multigit {
             }
         }
         Ok(())
+    }
+
+    fn ui(&self, filter: Option<&Vec<Filter>>) -> Result<()> {
+        for repository in self.all_repositories(filter)?.iter() {
+            println!("Opening git ui for {}", repository.path.to_str().unwrap());
+            //open_in_git_ui(&repository.path)?;
+        }
+        Ok(())
+    }
+}
+
+#[derive(clap::ValueEnum, Clone, Debug, Serialize)]
+enum Filter {
+    Dirty,
+}
+
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+enum EntryState {
+    Clean,
+    Dirty,
+}
+
+struct RepositoryState {
+    entries: HashSet<EntryState>,
+}
+
+fn open_in_git_ui(path: &Path) -> Result<()> {
+    let editor = "gitup";
+    let status = std::process::Command::new(editor).arg(path).status()?;
+    if !status.success() {
+        return Err(anyhow!("Failed to open git ui"));
+    }
+    Ok(())
+}
+
+fn find_repositories(path: &Path) -> Result<Vec<PathBuf>> {
+    let mut repositories = Vec::new();
+    let walker = WalkDir::new(path).into_iter().filter_entry(|e| {
+        e.file_type().is_dir() && !is_hidden(e.path()) && e.path().file_name().unwrap() != ".git"
+    });
+    for entry in walker {
+        let entry = entry?;
+        if is_git_repository(entry.path()) {
+            let path = entry.path();
+            repositories.push(path.to_path_buf());
+            // walker.skip_current_dir();
+        }
+    }
+    Ok(repositories)
+}
+
+fn is_git_repository(path: &Path) -> bool {
+    path.join(".git").exists()
+}
+
+fn is_hidden(path: &Path) -> bool {
+    path.file_name().unwrap().to_str().unwrap().starts_with(".")
+}
+
+fn noneify<T>(v: &Vec<T>) -> Option<&Vec<T>> {
+    if v.is_empty() {
+        None
+    } else {
+        Some(v)
     }
 }
