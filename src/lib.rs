e06e60b77a6c82a1da0ee348436b97626c41fd95
Jonathan Wight <schwa@schwa.io>
2024-09-17 10:52:50 -0700

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -462,7 +462,9 @@ pub struct RepositoryState {
 /// Opens the configured Git UI for a given repository path.
 pub fn open_in_git_ui(path: &Path) -> Result<()> {
     let editor = "gitup";
-    let status = std::process::Command::new(editor).current_dir(path).status()?;
+    let status = std::process::Command::new(editor)
+        .current_dir(path)
+        .status()?;
     if !status.success() {
         return Err(anyhow!("Failed to open git ui"));
     }
